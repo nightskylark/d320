ca7b62fd98a9adb1783168b759fac289cad4abbd
nightskylark <eugen.zha@gmail.com>
2025-03-01 22:23:38 +0200

    Migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'empty-module',
+      buffer: 'buffer',
+      process: 'process/browser',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+});
