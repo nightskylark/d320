cdc501415c5c390ae3c391785ff024f46e032263
nightskylark <eugen.zha@gmail.com>
2025-03-01 18:26:53 +0200

    Remove obsolete upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Upload from "./components/Upload";
 import Auth from "./components/Auth";
 import AddEnemy from "./components/AddEnemy";
 import EnemyList from "./components/EnemyList";
@@ -17,7 +16,6 @@ function App() {
       <h1>d320</h1>
       <Auth />
       <h1>Catalog</h1>
-      <Upload />
       <AddEnemy onEnemyAdded={handleEnemyAdded} />
       <EnemyList enemies={enemies} />
     </div>
