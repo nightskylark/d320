7ba54af5aeeeed347beb9c7a1f8dba821811a5e1
zhavoronkov.evgeny <nightskylark@users.noreply.github.com>
2025-06-04 12:35:27 +0300

    Fix catalog heading test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./components/EnemyList', () => () => <div data-testid="enemy-list" />);
+jest.mock('./components/Header', () => () => <header />);
+jest.mock('./firebase', () => ({
+  enemiesCollection: {},
+  auth: {},
+  db: {}
+}));
+jest.mock('firebase/firestore', () => ({
+  onSnapshot: jest.fn(() => () => {})
+}));
+jest.mock('firebase/auth', () => ({
+  onAuthStateChanged: jest.fn(() => () => {})
+}));
+
 test('renders catalog heading', () => {
   render(<App />);
   const heading = screen.getByText(/Каталог противников/i);
