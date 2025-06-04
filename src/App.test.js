7a8ae190268019854865da18d494effbfc2ea402
zhavoronkov.evgeny <nightskylark@users.noreply.github.com>
2025-06-04 12:25:59 +0300

    Update test for catalog heading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders catalog heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Каталог противников/i);
+  expect(heading).toBeInTheDocument();
 });
