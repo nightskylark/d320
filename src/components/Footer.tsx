8b59e763e94cd0d6ac3bbe5b65bc85ba197cefaf
nightskylark <eugen.zha@gmail.com>
2025-06-05 18:26:58 +0300

    Polish

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer: React.FC = () => (
   <footer className="text-center text-gray-400 p-4 flex items-center justify-center gap-2">
-    <span>d320, Eugen Zha,</span>
+    <span>Eugen Zha</span>
     <a href="https://t.me/d320stories" target="_blank" rel="noopener noreferrer" className="flex items-center gap-1 link">
       <img src="./telegram.svg" alt="Telegram" className="w-5 h-5" />
-      https://t.me/d320stories
+      t.me/d320stories
     </a>
   </footer>
 );
